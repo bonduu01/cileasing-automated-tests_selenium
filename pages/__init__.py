55dd02c8dc2b4426747efc578758506ddda82a1f
Peter Molokwu <bonduu01@gmail.com>
2026-02-01 18:03:00 +0100

    Test_cases:2026-01-12:07:19:00PM

--- pages/__init__.py
+++ pages/__init__.py
@@ -7,8 +7,6 @@ from pages.home_page import HomePage
 from pages.login_page import LoginPage
 from pages.self_service_page import SelfServicePage
 from pages.edit_self_service_page import EditSelfServicePage
-from pages.add_bank_details_page import AddBankDetailsPage
-from pages.edit_bank_details_page import EditBankDetailsPage
 from pages.add_emergency_contact_page import AddEmergencyContactPage
 from pages.edit_emergency_contact_page import EditEmergencyContactPage
 from pages.add_bvn_page import AddBvnPage
